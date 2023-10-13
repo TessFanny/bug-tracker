4fbf40d692cff25366adfabd0dd366376a1a73ff
TessFanny <fanitess@yahoo.fr>
2023-10-13 11:57:09 +0200

    Remove server code

--- client/src/components/BigSideBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BigSideBar = () => {
-  return (
-    <aside className=' h-screen bg-stone-600 w-10'>BigSideBar</aside>
-  )
-}
-
-export default BigSideBar
--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import Register from "./Register";
-import Admin from "./Admin";
-import Dashboard from "./Dashboard";
-import Tickets from "./Tickets";
-export {Register, Admin, Dashboard, Tickets}
