a13ea646a5fee464da1db184b708f27053ac81bd
TessFanny <fanitess@yahoo.fr>
2023-10-15 23:32:39 +0200

    removing user from project

--- server/app/controller/projectController.js
+++ server/app/controller/projectController.js
@@ -111,10 +111,13 @@ const projectController = {
  // delete an user working on a project 
  deleteUserOnProject: async(req, res, next)=>{
   try {
-    const projectModel = new Project(req.body)
-    const project = await  projectModel.findByPk(+req.params.project_id);
+    const { project_id} = req.params
+    const {user_id} = req.params
+    console.log(project_id);
+    const projectModel = new Project(req.body);
+    const project = await  projectModel.findByPk(project_id);
     if (project) {
-      const userOnProjectToDelete = await projectModel.deleteUserFromProjectModel(+req.params.project_id, +req.params.user_id)
+      const userOnProjectToDelete = await projectModel.deleteUserFromProjectModel(project_id, user_id)
       console.log(userOnProjectToDelete);
       res.status(200).json(userOnProjectToDelete);
     } else {
@@ -124,7 +127,8 @@ const projectController = {
     }
     
   } catch (error) {
-    
+    console.error(error);
+      next(error);
   }
  },
   updateProject: async (req, res, next) => {
